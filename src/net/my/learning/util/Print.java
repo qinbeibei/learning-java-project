d03bd953abc833cfc761d5c39cfe79409ef314c7
qinbeibei <qinbeibei44@126.com>
2020-12-26 19:16:51 +0800

    从idea推送

--- src/net/my/learning/util/Print.java
+++ src/net/my/learning/util/Print.java
@@ -1,7 +1,12 @@
 package net.my.learning.util;
 
+//
 public class Print {
 	public static void print(Object x) {
 		System.out.println(x);
+	}
+
+	public static void print(){
+		System.out.println();
 	}
 }
